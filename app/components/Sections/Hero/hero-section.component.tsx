c73c6bda5c15a5829acfb6df4b5efac56b7f6b29
luxejs <luxe.jsx@gmail.com>
2024-03-27 12:49:27 -0400

    update hero

--- app/components/Sections/Hero/hero-section.component.tsx
+++ app/components/Sections/Hero/hero-section.component.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import { GridLines, GradientLayer } from '../..'
 
 export const HeroSection = () => {
@@ -14,21 +15,33 @@ export const HeroSection = () => {
       />
 
       <div className='max-w-7xl py-0 lg:px-8 lg:py-40'>
-        <div className='mx-auto flex max-w-2xl flex-shrink-0 flex-col items-center justify-center lg:mx-0 lg:max-w-xl '>
+        <div className='mx-auto flex max-w-2xl flex-shrink-0 flex-col items-center justify-center lg:mx-0 lg:max-w-xl'>
+        <Image
+                // src='/comai-webp.webp'
+                src='/commune-gif.gif'
+                width={200}
+                height={200}
+                alt='commune logo'
+                // unoptimized
+                // className='ml-2 h-12'
+                loading='lazy'
+              />
           <div className='flex items-center'>
+            
             <h1 className='animate-scale-down-md bg-gradient-to-br from-sky-300 to-white bg-clip-text text-5xl font-bold tracking-tight text-transparent'>
               commune ai
             </h1>
+
           </div>
           <p className='mt-6 animate-scale-down-md text-center text-xl font-extralight leading-relaxed tracking-tight text-gray-100 lg:text-center'>
-            Renovating the way we build software.
+            Renovating the way we build software for <span className='inline-block bg-gradient-to-r from-yellow-200 via-red-400 to-indigo-400 bg-clip-text text-1xl text-transparent'>everyone</span>.
           </p>
           <div className='z-10 mt-10 flex animate-scale-down-md items-center gap-x-6'>
             <Link
               href='#discord'
               className='font-base rounded-3xl  bg-gradient-to-br from-indigo-500 to-blue-800 px-3.5 py-2.5 text-sm text-white shadow-sm hover:animate-scale-up-sm  hover:from-indigo-500 hover:to-blue-900 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-400'
             >
-              <span aria-label='Get started with Commune AI'>Start</span>
+              <span aria-label='Get started with Commune AI'>Get Started</span>
             </Link>
           </div>
         </div>
