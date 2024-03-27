2831541191a8ef332fd03315f8bbf2ab9e5ce1f0
luxejs <luxe.jsx@gmail.com>
2024-03-27 15:12:53 -0400

    transparent widgets with animated online icon

--- app/components/DiscordWidget/discord-widget.component.tsx
+++ app/components/DiscordWidget/discord-widget.component.tsx
@@ -19,9 +19,9 @@ export const DiscordWidget = async () => {
 
   if (data)
     return (
-      <div className='lg:max-w-11/12 mt-8 flex w-full flex-col items-center justify-between rounded-2xl bg-gradient-to-br from-indigo-600 to-gray-400 p-6 shadow-xl md:flex-row md:items-center lg:flex-col lg:items-center lg:px-6 xl:flex-row'>
+      <div className='lg:max-w-11/12 mt-8 flex w-full flex-col items-center justify-between rounded-2xl bg-transparent p-6 shadow-xl md:flex-row md:items-center lg:flex-col lg:items-center lg:px-6 xl:flex-row'>
         <div className='flex flex-col items-center justify-center md:flex-row lg:flex-col xl:flex-row'>
-          <div className='mb-2 flex h-12 w-12 items-center justify-center rounded-2xl bg-gradient-to-br from-green-900/70 to-gray-900/50 p-1.5 shadow-xl md:mb-0 md:mr-3 lg:mb-2 lg:mr-0 xl:mb-0 xl:mr-3'>
+          <div className='mb-2 flex h-12 w-12 items-center justify-center rounded-2xl bg-gradient-to-br from-blue-900/70 to-gray-900/50 p-1.5 shadow-xl md:mb-0 md:mr-3 lg:mb-2 lg:mr-0 xl:mb-0 xl:mr-3'>
             <Image
               src='/discord-icon-white.svg'
               priority
@@ -36,6 +36,8 @@ export const DiscordWidget = async () => {
             <div className='flex flex-col items-center sm:flex-row'>
               <div className='flex items-center'>
                 <span className='mr-1 inline-block h-3 w-3 rounded-full bg-green-500' />
+                <span className='absolute mr-1 inline-block h-3 w-3 rounded-full animate-ping bg-green-500' />
+
                 <span className='text-shad w-auto text-sm text-gray-100'>
                   {data.presence_count} Online
                 </span>
@@ -50,8 +52,8 @@ export const DiscordWidget = async () => {
           target='_blank'
           rel='noopener noreferrer'
         >
-          <span className='absolute right-0 mt-[10px] inline-block h-3 w-3 animate-ping rounded-full bg-gradient-to-br from-indigo-500 to-gray-100 md:mt-0 lg:mt-2 xl:mt-0' />
-          <span className='absolute right-0 mt-[10px] inline-block h-3 w-3 rounded-full bg-gradient-to-br from-indigo-500 to-gray-100 md:mt-0 lg:mt-2 xl:mt-0' />
+          {/* <span className='absolute right-0 mt-[10px] inline-block h-3 w-3 animate-ping rounded-full bg-gradient-to-br from-indigo-500 to-gray-100 md:mt-0 lg:mt-2 xl:mt-0' /> */}
+          {/* <span className='absolute right-0 mt-[10px] inline-block h-3 w-3 rounded-full bg-gradient-to-br from-indigo-500 to-gray-100 md:mt-0 lg:mt-2 xl:mt-0' /> */}
           <button className='mt-3 rounded-3xl bg-gradient-to-br from-indigo-500 to-blue-800 px-4 py-3 text-sm font-medium text-white hover:from-indigo-500 hover:to-blue-900 md:mt-0 lg:mt-3 xl:mt-0'>
            Join Us
           </button>
