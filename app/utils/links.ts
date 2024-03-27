34941bf426c4bfe2cf37dc5f03a8ef31bf353dff
luxejs <luxe.jsx@gmail.com>
2024-03-27 12:48:19 -0400

    updated links

--- app/utils/links.ts
+++ app/utils/links.ts
@@ -3,6 +3,7 @@ export const links = {
   docs: '/docs/getting-started/intro',
   whitepaper: 'https://ai-secure.github.io/DMLW2022/assets/papers/7.pdf',
   github: 'https://github.com/commune-ai',
-  discord: 'https://discord.com/invite/A8JGkZ9Dmm',
+  discord: 'https://discord.gg/communeai',
   x: 'https://twitter.com/communeaidotorg',
+  comhub: 'https://www.comhub.app/',
 }
