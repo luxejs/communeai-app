ed45a41d7e0bcc614b36be269bc13cb6c54517e3
luxejs <luxe.jsx@gmail.com>
2024-06-13 02:03:58 -0400

    add border to generic section

--- app/components/Sections/GenericSection/generic-section.component.tsx
+++ app/components/Sections/GenericSection/generic-section.component.tsx
@@ -32,7 +32,8 @@ export const GenericSection = ({
   return (
     <section
       id={sectionName}
-      className={`relative m-6 w-11/12 max-w-screen-xl overflow-hidden rounded-3xl bg-gray-800/40 ${color} px-6 py-16 text-center shadow-2xl md:p-12 lg:m-8 lg:p-24 `}
+      className={`relative m-6 w-11/12 max-w-screen-xl overflow-hidden rounded-3xl bg-gray-800/40 ${color} px-6 py-16 text-center shadow-2xl md:p-12 lg:m-8 lg:p-24 border border-white border-opacity-[0.23]
+      `}
     >
       <GridLines />
       <GradientLayer
@@ -42,25 +43,25 @@ export const GenericSection = ({
 
       <div className='mx-auto flex max-w-7xl flex-col items-center justify-center text-left'>
         <div className='mx-auto max-w-2xl'>
-        <Image
-                src={image}
-                // src='/commune-gif.gif'
-                width={130}
-                height={130}
-                alt='commune logo'
-                // unoptimized
-                // className='ml-2 h-12'
-                loading='lazy'
-                className='mx-auto'
-              />
-          <h2 className='text-center text-base font-semibold leading-7 text-indigo-400'>
-            {subtitle}
-          </h2>
-          <p className='mt-2 text-center text-3xl font-bold tracking-tight text-white sm:text-4xl'>
-            {title}
-          </p>
+            <Image
+              src={image}
+              // src='/commune-gif.gif'
+              width={130}
+              height={130}
+              alt='commune logo'
+              // unoptimized
+              // className='ml-2 h-12'
+              loading='lazy'
+              className='mx-auto'
+            />
+            <h2 className='text-center text-base font-semibold leading-7 text-indigo-400'>
+              {subtitle}
+            </h2>
+            <p className='mt-2 text-center text-3xl font-bold tracking-tight text-white sm:text-4xl'>
+              {title}
+            </p>
         </div>
-        <dl className='mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 text-base leading-7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gap-x-16'>
+          <dl className='mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 text-base leading-7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gap-x-16'>
           {features.map((feature, index) => (
             <div key={index} className='relative pl-9'>
               <dt className='inline font-semibold text-white'>
@@ -71,8 +72,8 @@ export const GenericSection = ({
               </dt>
               <dd className='inline'>{feature.description}</dd>
             </div>
-          ))}
-        </dl>
+        ))}
+          </dl>
       </div>
     </section>
   )
