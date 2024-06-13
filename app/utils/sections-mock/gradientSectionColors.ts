54aeb6e694a590f8ec59a7e8c59b158a6f57788c
luxejs <luxe.jsx@gmail.com>
2024-06-13 02:01:00 -0400

    potential colours for cards, only using comhub violet

--- app/utils/sections-mock/gradientSectionColors.ts
+++ app/utils/sections-mock/gradientSectionColors.ts
@@ -6,3 +6,29 @@ export const gradientSectionColors = {
   pink: 'from-[#FF70DB] via-[#FFA3E8]',
   cyan: 'from-cyan-400 via-cyan-200',
 }
+
+export const gradientModuleColors = {
+  // blue: 'from-[#077BFF] via-[#3C96FF]',
+  // green: 'from-[#60F770] via-[#79F786]',
+  // yellow: 'from-[#FFEF3B] via-[#FFF36E]',
+  // red: 'from-[#FF292A] via-[#FF5C5C]',
+  // pink: 'from-[#FF70DB] via-[#FFA3E8]',
+  // cyan: 'from-cyan-400 via-cyan-200',
+  // black: 'from-black via-gray-800',
+  // purple: 'from-[#7F00FF] via-[#E100FF]',
+  // // orange: 'from-[#FF7E00] via-[#FFAE42]',
+  // // teal: 'from-[#20B2AA] via-[#66CDAA]',
+  // // lime: 'from-[#AFFF33] via-[#CFFF5A]',
+  // indigo: 'from-[#4B0082] via-[#6A5ACD]',
+  // violet: 'from-[#EE82EE] via-[#DDA0DD]',
+  // // gold: 'from-[#FFD700] via-[#FFEC8B]',
+  // magenta: 'from-[#FF00FF] via-[#FF77FF]',
+  // lavender: 'from-[#E6E6FA] via-[#D8BFD8]',
+  // coral: 'from-[#FF7F50] via-[#FFA07A]',
+  // olive: 'from-[#808000] via-[#BDB76B]',
+  // maroon: 'from-[#800000] via-[#B03060]',
+  // navy: 'from-[#000080] via-[#4682B4]',
+  // beige: 'from-[#F5F5DC] via-[#FFF8DC]',
+  // white: 'from-white via-gray-200',
+  violet: 'from-[#6366F1] via-black'
+}
